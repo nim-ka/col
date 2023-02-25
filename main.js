851cde87848655cba051d8826f2a7c22e634077f
GijsvandenHoven <gijsvandenhoven@beseder.nl>
2023-02-25 01:47:14 +0100

    merge branches
    initially added:
    time estimation while running
    total runtime printout

--- main.js
+++ main.js
@@ -118,15 +118,68 @@ goodyaw = function(m) {
 let start = +process.argv[2]
 let end = +process.argv[3]
 
+const diff = Math.abs(start - end);
+
 if (!start || !end) {
 	repl.start("> ")
 } else {
 	let l = 0
 
+	const startTime = process.hrtime.bigint(); // Monotonic clock, reports in nanoseconds.
+	const timePoints = Array.from({ length: diff }, () => 0n);
 	for (let x = start; x > end; x--) {
 		let p = testx(x, goodyaw)
 		console.log(x, l += p.length, p.length)
-	        fs.writeFileSync(`tests/points${x}.txt`, JSON.stringify(p))
+		fs.writeFileSync(`tests/points${x}.txt`, JSON.stringify(p))
+
+		// Boilerplate for estimating completion
+		const index = start - x;
+		timePoints[index] = process.hrtime.bigint();
+		const estimatedTotalTimeMS = Number(interpolateEndTime(startTime, timePoints) / 1_000_000n);
+		const ratioRemaining = (diff - index - 1) / diff;
+		console.log(`Estimated time remaining: ${timeString(estimatedTotalTimeMS * ratioRemaining)}`);
 	}
+	const endTime = process.hrtime.bigint();
+	console.log(`Segment complete. Total time: ${timeString((endTime - startTime) / 1_000_000n)}`);
 }
 
+/**
+ * Given a partially completed array of timePoints,
+ * Accumulates all non-zero time points into an array,
+ * So that a rate of progress is estimated.
+ *
+ * Using the length of the array, this rate of progress is used to estimate when the process is 'done'.
+ * Where 'done' means 'when the last array element would be written'.
+ *
+ * @param startTime {bigint} starting time of the process
+ * @param timePoints {bigint[]} array of measurements of time. Incomplete values are represented as 0.
+ *
+ * @return {bigint} Estimated total time in nanoseconds.
+ */
+function interpolateEndTime(startTime, timePoints) {
+	// Not entirely accurate due to integer division in the average computation,
+	// but it shouldn't matter because nanoseconds are overly accurate anyway.
+	const rateOfProgress = timePoints
+		.filter(v => v > 0n) // for measurements
+		.map((v, i, array) => v - (array[i-1] ?? startTime)) // get differences
+		.reduce((sum, v, _, { length }) => sum + (v / BigInt(length)), 0n); // compute average of differences.
+
+	return BigInt(timePoints.length) * rateOfProgress;
+}
+
+function timeString(timeMS) {
+	const h = Math.floor(timeMS / (1000 * 3600));
+	timeMS -= h * 1000 * 3600;
+
+	const m = Math.floor(timeMS / (1000 * 60));
+	timeMS -= m * 1000 * 60;
+
+	const s = Math.floor(timeMS / (1000));
+	timeMS -= s * 1000;
+
+	const ms = Math.floor(timeMS);
+
+	const pad = (n) => n > 9 ? n.toString() : '0' + n.toString();
+
+	return `${h}:${pad(m)}:${pad(s)}.${ms}`;
+}
